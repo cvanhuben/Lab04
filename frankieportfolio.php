9f2eb8860fecbbf172ab835eb030e8408ee7b843
fromero2 <32085702+fromero2@users.noreply.github.com>
2017-09-20 20:12:08 -0400

    Frankie's lab2
    frankies portfolio

---
+++ frankieportfolio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8 />
+    <title>Frankie Romero's Portfolio</title>
+	
+	
+</head>
+<body>
+<h1>Frankie Romero</h1>
+    
+    <h2>Who I am</h2>
+    
+    <p>I am a 20 year old female from the Bronx, NY. I attend the University of Rochester, pursuing a bachelor's degree in Psychology. I hope to further attend graduate school in New York City, studying social work. I dream of being a social worker for child welfare.</p>
+    
+    <h2>Work Experience</h2>
+    
+    I've worked at non profit organizations to help minimize the minority opportunity gap. I like to help students apply for college,and apply themselves in college as well. I hope to create my own not for profit to continue to help minority students in inner cities, succeed in ways I have and have not. 
+    
+    <h2>Skills</h2>
+    
+    <p>Basic programming skills in Python and JavaScript; light web design proficiency
+    Proficient in Microsoft Office: Word, Excel, PowerPoint.
+    Fluent in English with basic proficiency of Spanish</p>
+    
+
+ 
+</body>
+</html>
