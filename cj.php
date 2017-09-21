4c68a1a3cf1b39159aa0e1eeee340039bc80862e
CJ Van Huben <cvanhube@u.rochester.edu>
2017-09-20 21:29:32 -0400

    This is CJ's PHP file
    CJ's PHP File

---
+++ cj.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset = "utf-8">
+	<title> Assignment 2 - Christopher Van Huben </title>
+
+	</head>
+
+	<body>
+
+	<header>	
+	<h1>Christopher Van Huben</h1>
+	<h2>Call me CJ</h2>
+	</header>
+
+	<article>
+
+			<h3>About me</h3>
+				<p> I was born in Poughkeepsie, NY on October 24th. I am the youngest of two children. My mom's name is Margaret and my dad's name is Gary. My sister's name is Anne. I attended Arlington High School and graduated in 2014. 
+				</p>
+
+				<p>Currently I am a Senior studying Financial Economics. I am also trying to pick up a minor in Computer Science. I've been a TA for ECO 108. Currently I am a Peer Advisor for the ECON department. Next year I will be moving to Manhattan and taking a job with EY doing Financial Consulting. Before that I interned with SpaceX in Los Angeles in their Business Operations Procurement department. Prior to that I worked with BASF as a Financial Analyst.</p>
+
+
+
+			<h3>Current Classes</h3>
+			
+			<ul>
+				<li>CSC 172</li>
+				<li>CSC 174</li>
+				<li>CSC 175</li>
+				<li>CIS 240</li>
+			</ul>
+
+
+
+			<h3>Fun Facts</h3>
+			<ul>
+				<li>This first time I flew on a plane was when I was 20</li>
+				<li>I have two different sized pupils</li>
+				<li>I play saxophone and guitar better than I design websites</li>
+
+			</ul>
+
+
+	</article>
+
+	</body>
+	</html>
