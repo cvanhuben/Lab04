219def9dc3ecc5cfae7bd818b5dfcc1910f63d33
CJ Van Huben <cvanhube@u.rochester.edu>
2017-09-20 21:50:28 -0400

    Moved stuff around and created an index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+	</!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<title> The Bronx CSC 174 Project</title>
+	</head>
+	<body>
+	
+		<nav>
+			<a href="#">CJ</a>
+			<a href="#">Frankie</a>
+			<a href="#">Jien</a>
+
+		</nav>
+
+		<h1>The Bronx CSC 174 Project</h1>
+
+		<?php include "pages/cj.php" ?>
+		<?php include "pages/frankieportfolio.php" ?>
+
+
+
+	<footer>Copyright 2017 - The Bronx</footer>	
+	</body>
+	</html>
