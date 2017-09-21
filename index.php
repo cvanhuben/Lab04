64b268632705356a99fb4d075afe692e53ebbd66
Jigowa <jogawa@u.rochester.edu>
2017-09-21 12:53:57 -0400

    Homepage CSS

--- index.php
+++ index.php
@@ -1,35 +1,59 @@
-	</!DOCTYPE html>
-	<html>
+<!DOCTYPE html>
+<html>
 	<head>
 		<meta charset="utf-8">
-		<title> The Bronx CSC 174 Project</title>
+		<title>The Bronx</title>
 		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 		<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
 	</head>
 	<body>
 	
 		<nav>
+			<a href="index.php">Home</a>
 			<a href="pages/cj.php">CJ</a>
 			<a href="pages/frankieportfolio.php">Frankie</a>
 			<a href="pages/jien.php">Jien</a>
 
 		</nav>
-<div class ="textbox">
-		<h1>The Bronx CSC 174 Project</h1>
-
-		<!--
-
-		<div class ="textbox">
-		<?php include "pages/cj.php" ?>
-		<?php include "pages/frankieportfolio.php" ?>
-		</div>
+		<div class="textbox">
+			<h1>MEET THE BRONX TEAM</h1>
 	
-		-->
-</div>
-	<footer>
-	<p>CJ - Frankie - Jien</p>	
-	<p>Copyright 2017 - The Bronx</p>
-	</footer>	
+			<!--
+
+			<div class ="textbox">
+			<?php include "pages/cj.php" ?>
+			<?php include "pages/frankieportfolio.php" ?>
+			</div>
+
+			-->
+		</div>
+		<div class="previews">
+			<div name="cjPreview">
+				<h2>CJ Van Huben</h2>
+				<p>
+					I was born in Poughkeepsie, NY on October 24th. I am the youngest of two children. My mom's name is Margaret and my dad's name is Gary. My sister's name is Anne. I attended Arlington High School and graduated in 2014.
+				</p>
+				<a href="pages/cj.php">...Read more about CJ...</a>
+			</div>
+			<div name="frankiePreview">
+				<h2>Frankie Romero</h2>
+				<p>
+					I am a 20 year old female from the Bronx, NY. I attend the University of Rochester, pursuing a bachelor's degree in Psychology. I hope to further attend graduate school in New York City, studying social work. I dream of being a social worker for child welfare.
+				</p>
+				<a href="pages/frankieportfolio.php">...Read more about Frankie...</a>
+			</div>
+			<div name="jienPreview">
+				<h2>Jien Ogawa</h2>
+				<p>
+					I was born to a Japanese mother (Naomi) and an American father (Josh) on 29th October, 1996 in Tokyo, Japan. Shortly before my first birthday, my mother relocated permanently to America to give me the opportunity to be raised with my father and give me access to America's higher education system. I attended Colonie Central High School 2010-2014, and I am currently a senior at the University of Rochester.
+				</p>
+				<a href="pages/cj.php">...Read more about Jien...</a>
+			</div>
+		</div>
+		<footer>
+			<p>CJ - Frankie - Jien</p>	
+			<p>Copyright 2017 - The Bronx</p>
+		</footer>	
 	
 	</body>
-	</html>
+</html>
