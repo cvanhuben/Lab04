5dca4d2e3f34952914c6caf33e2e3a9eae7322c4
Jigowa <jogawa@u.rochester.edu>
2017-09-21 16:38:17 -0400

    Changed name to id

--- index.php
+++ index.php
@@ -27,26 +27,26 @@
 			-->
 		</div>
 		<div class="previews">
-			<div name="cjPreview">
+			<div id="cjPreview">
 				<h2>CJ Van Huben</h2>
 				<p>
 					I was born in Poughkeepsie, NY on October 24th. I am the youngest of two children. My mom's name is Margaret and my dad's name is Gary. My sister's name is Anne. I attended Arlington High School and graduated in 2014.
 				</p>
 				<a href="cj.php">...Read more about CJ...</a>
 			</div>
-			<div name="frankiePreview">
+			<div id="frankiePreview">
 				<h2>Frankie Romero</h2>
 				<p>
 					I am a 20 year old female from the Bronx, NY. I attend the University of Rochester, pursuing a bachelor's degree in Psychology. I hope to further attend graduate school in New York City, studying social work. I dream of being a social worker for child welfare.
 				</p>
 				<a href="frankieportfolio.php">...Read more about Frankie...</a>
 			</div>
-			<div name="jienPreview">
+			<div id="jienPreview">
 				<h2>Jien Ogawa</h2>
 				<p>
 					I was born to a Japanese mother (Naomi) and an American father (Josh) on 29th October, 1996 in Tokyo, Japan. Shortly before my first birthday, my mother relocated permanently to America to give me the opportunity to be raised with my father and give me access to America's higher education system. I attended Colonie Central High School 2010-2014, and I am currently a senior at the University of Rochester.
 				</p>
-				<a href="cj.php">...Read more about Jien...</a>
+				<a href="jien.php">...Read more about Jien...</a>
 			</div>
 		</div>
 		<footer>
